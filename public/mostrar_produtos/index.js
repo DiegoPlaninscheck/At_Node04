a7cd29e087cc47a97469b62c7fc0637a17c7e042
Diego <92130153+DiegoPlaninscheck@users.noreply.github.com>
2022-11-25 20:32:43 -0300

    feat: edicao e exclusao

--- public/mostrar_produtos/index.js
+++ public/mostrar_produtos/index.js
@@ -11,7 +11,8 @@ async function criarProdutos() {
 
   for (const produto of produtos) {
     const div = document.createElement("div");
-    div.id = "info";
+    div.classList.add("info");
+    div.id = produto.id;
 
     const spanTipo = document.createElement("span");
     spanTipo.innerText = `Tipo: ${produto.tipo}`;
@@ -19,15 +20,36 @@ async function criarProdutos() {
     spanNome.innerText = `Nome: ${produto.nome}`;
     const spanQuantidade = document.createElement("span");
     spanQuantidade.innerText = `Quantidade: ${produto.quantidade}`;
-    const imgEdicao = document.createElement("img");
-    imgEdicao.src = "../assets/pencil.png";
+    const edicao = document.createElement("span");
+    edicao.innerText = "Editar";
+    edicao.style.cursor = "pointer";
+    // edicao.onclick = editar(produto.id);
 
-    containerGeral.appendChild(div);
+    const exclusao = document.createElement("span");
+    exclusao.innerText = "Excluir";
+    exclusao.style.cursor = "pointer";
+    exclusao.addEventListener("toggle", excluir(produto.id));
+
     div.appendChild(spanTipo);
     div.appendChild(spanNome);
     div.appendChild(spanQuantidade);
-    div.appendChild(imgEdicao);
+    div.appendChild(edicao);
+    div.appendChild(exclusao);
+    containerGeral.appendChild(div);
   }
 }
+
+const editar = (id) => {
+  console.log(id);
+  // const div = document.getElementById("")
+
+  // if(){
+
+  // }
+};
+
+const excluir = (id) => {
+  console.log(id);
+};
 
 criarProdutos();
